798f8596b207774c94e9c5025974bf55cd7fb886
Johndevtech <john.njoroge@student.moringaschool.com>
2022-02-14 14:40:18 +0300

    test:deployment test

---
+++ JS/script.js
@@ -0,0 +1,30 @@
+// document.getElementByTagName("button").style.display="none";
+
+// var audioBtn = document.getElementByClassName("audio");
+
+function vid1(){
+    var btn1 = document.getElementById("video1");
+    if (btn1.style.display === "none"){
+        btn1.style.display = "block";
+    } else{
+        btn1.style.display = "none";
+    }
+};
+
+function vid2(){
+    var btn2 = document.getElementById("video2");
+    if (btn2.style.display === "none"){
+        btn2.style.display = "block";
+    } else{
+        btn2.style.display = "none";
+    }
+};
+
+function vid3(){
+    var btn3 = document.getElementById("video3");
+    if (btn3.style.display === "none"){
+        btn3.style.display = "block";
+    } else{
+        btn3.style.display = "none";
+    }
+};
